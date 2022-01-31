d663a453f204ba9e4aa124309c4b7d6befa88f46
Bipin Sahu <bipinsahu5@gmail.com>
2022-02-01 01:43:04 +0530

    JS Training

--- pract.js
+++ pract.js
@@ -1,11 +1,34 @@
-const request = new XMLHttpRequest();
-request.addEventListener('readystatechange'){
-    if(request.readyState === 4){
-        console.log(request.responseText);
-    }
-}
-request.open('GET','https://jsonplaceholder.typicode.com/todos/')
+const getList = (callback)=>{
+    const request = new XMLHttpRequest();
+request.addEventListener('readystatechange',()=>{
+    
+        if(request.readyState === 4 && request.status === 200){
+            callback(request.status ,request.responseText);
+        }
+        else if(request.readyState===4){
+            callback(request.status,undefined);
+        }
+    
+})
+request.open('GET','https://jsonplaceholder.typicode.com/todos/1')
 request.send();
+}
 
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
 
+getList((stat,data)=>{
+   console.log('Callback Fired')
+    if(stat === 200)
+    console.log(data);
+    else
+    console.log('Couldnt fetch')
+})
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
 

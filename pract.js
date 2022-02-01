e9f624709caf4f85c4ed9bd007e93695aca99e73
Bipin Sahu <bipinsahu5@gmail.com>
2022-02-01 16:00:46 +0530

    Callback Practice

--- pract.js
+++ pract.js
@@ -3,32 +3,17 @@ const getList = (callback)=>{
 request.addEventListener('readystatechange',()=>{
     
         if(request.readyState === 4 && request.status === 200){
-            callback(request.status ,request.responseText);
+            const data = JSON.parse(request.responseText)
+            callback(request.status ,data);
         }
         else if(request.readyState===4){
             callback(request.status,undefined);
         }
     
 })
-request.open('GET','https://jsonplaceholder.typicode.com/todos/1')
+request.open('GET','pr.json')
 request.send();
 }
 
-console.log(1);
-console.log(2);
-console.log(3);
-console.log(4);
 
-getList((stat,data)=>{
-   console.log('Callback Fired')
-    if(stat === 200)
-    console.log(data);
-    else
-    console.log('Couldnt fetch')
-})
-
-console.log(1);
-console.log(2);
-console.log(3);
-console.log(4);
 

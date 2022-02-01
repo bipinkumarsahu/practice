ab29cea67b7c3febe5ff51d87d23e2197ca03178
Bipin Sahu <bipinsahu5@gmail.com>
2022-02-01 18:35:29 +0530

    Fetch API

---
+++ pract2.js
@@ -0,0 +1,9 @@
+fetch('jsons/users.json').then(response=>{
+   console.log('resolved',response)
+    return response.json()
+}).then(data=>{
+    console.log(data);
+})
+.catch(err=>{
+    console.log(err)
+})

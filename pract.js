dc14c9bf56524e18614fd9a4e8c4959a3c684d2c
Bipin Sahu <bipinsahu5@gmail.com>
2022-02-01 17:54:58 +0530

    Promise Chaininng

--- pract.js
+++ pract.js
@@ -1,19 +1,41 @@
-const getList = (callback)=>{
+const getList = (resource) => {
+  return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
-request.addEventListener('readystatechange',()=>{
-    
-        if(request.readyState === 4 && request.status === 200){
-            const data = JSON.parse(request.responseText)
-            callback(request.status ,data);
-        }
-        else if(request.readyState===4){
-            callback(request.status,undefined);
-        }
-    
-})
-request.open('GET','pr.json')
-request.send();
-}
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject('error getting resource');
+      }
+    });
+
+    request.open("GET", resource);
+    request.send();
+  });
+};
 
 
+getList('pr.json').then(data=>{
+    console.log('promise 1 resolved',data);
+    return getList('qr.json');
+}).then(data=>{
+    console.log('promise 2 resolved',data);
+    return getList('sr.json');
+}).then(data=>{
+    console.log('promise 3 resolved',data);
+}).catch((stat)=>{console.log('error occured',stat)});
+// const getSomething = ()=>{
 
+//     return new Promise((resolve,reject)=>{
+//         resolve('some data');
+//         //reject('some error');
+
+//     })
+// }
+
+// getSomething().then((data)=>{
+//     console.log(data)
+// }).catch((err)=>{
+//     console.log(err);
+// });
